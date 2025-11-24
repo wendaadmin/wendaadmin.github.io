23125710e03b11092a8e6647bd461b5694f9a48e
psh006 <psh006@qq.com>
2025-11-24 08:21:32 +0000

    update 2025-11-24 08:21:30

---
+++ static/router-login.89bf0fb5.js
@@ -0,0 +1,1 @@
+import{aq as t,ad as o,ag as e}from"./entry.f2cf7577.js";const i=t((r,a)=>{if(o().getToken){if(r.path==="/")return e("/questionnaireSurveyForm")}else return e("/login")});export{i as default};
