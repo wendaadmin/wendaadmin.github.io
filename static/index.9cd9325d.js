61225f548305577a8cea1fa9fd95a31f7eee594c
psh006 <psh006@qq.com>
2025-11-25 03:23:21 +0000

    update 2025-11-25 03:23:21

---
+++ static/index.9cd9325d.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.da776960.js";const o={};function r(t,s){return n(),c("div",null,"index")}const a=e(o,[["render",r]]);export{a as default};
---
+++ static/router-login.aac6a615.js
@@ -0,0 +1,1 @@
+import{aq as t,ad as o,ag as e}from"./entry.da776960.js";const i=t((r,a)=>{if(o().getToken){if(r.path==="/")return e("/questionnaireSurveyForm")}else return e("/login")});export{i as default};
