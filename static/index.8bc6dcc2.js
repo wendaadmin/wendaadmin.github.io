b8aaadf02faffaf8a606300b19d314ed35aabd94
psh006 <psh006@qq.com>
2025-11-25 06:52:36 +0000

    update 2025-11-25 06:52:36

---
+++ static/index.8bc6dcc2.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.dd691c49.js";const o={};function r(t,s){return n(),c("div",null,"index")}const a=e(o,[["render",r]]);export{a as default};
---
+++ static/router-login.521ace9e.js
@@ -0,0 +1,1 @@
+import{ar as t,ae as o,ah as e}from"./entry.dd691c49.js";const i=t((r,a)=>{if(o().getToken){if(r.path==="/")return e("/questionnaireSurveyForm")}else return e("/login")});export{i as default};
