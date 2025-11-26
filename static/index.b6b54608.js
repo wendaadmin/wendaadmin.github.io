301530bcb48303bb7d518a18a8078145cdbc85d3
psh006 <psh006@qq.com>
2025-11-26 04:04:31 +0000

    update 2025-11-26 04:04:31

---
+++ static/index.b6b54608.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.d462ef47.js";const o={};function r(t,s){return n(),c("div",null,"index")}const a=e(o,[["render",r]]);export{a as default};
---
+++ static/router-login.e309fc0f.js
@@ -0,0 +1,1 @@
+import{as as t,ad as s,ag as e}from"./entry.d462ef47.js";const i=t((r,a)=>{if(s().getToken){if(r.path==="/")return e("/nameList")}else return e("/login")});export{i as default};
